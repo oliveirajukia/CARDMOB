53f85639c9cdac006bccf1a2c960b6fa4615d661
Julia Oliveira <juliaoliveira.amora@gmail.com>
2025-03-25 09:06:46 -0300

    Add TodoList component

---
+++ Etapa1/react-exemplos/src/components/TodoList.jsx
@@ -0,0 +1,79 @@
+import React, { use, useState } from "react";
+
+const TodoList = ({ name }) => {
+    const [todos, setTodo] = useState([]);
+    const [task, setTask] = useState ("");
+    const [editingId, setEditingId] = useState(null);
+    const [editingText, setEditingText] = useState("");
+
+const addTodo = () => {
+    if (task.trim() === "") return;
+    setTodo([...todos, {id: Date.now, text: task}]);
+    setTask("");
+}
+
+    const startEditing = (id, text) => {
+        setEditingId (id);
+        setEditingText(text);
+    }
+
+    // Update
+    const saveEdit = () => {
+        setTodo(
+            todos.map((todo) => 
+                todo.id == editingId ? {...todo, text: editingText} : todo
+            )
+        );
+        setEditingId(null);
+        setEditingText("");
+    }
+
+    // Delete
+    const deleteTodo = (id) => {
+        setTodo(todos.filter((todo) => todo.id !== id));
+    }
+    return (
+        <div style={{ textAlign: "center", margintTop: "50px" }}>
+            <h2>A fazer: {name}</h2>
+            <input
+             type="text"
+             value={task}
+             onChange={(event) => setTask(event.target.value) }
+             placeholder="Escreva sua tarefa..."
+            />
+            <button onClick={addTodo}>Incluir tarefa</button>
+            <url style={{ listStyle: "none", padding:0 }}>
+                {todos.map((todo) =>(
+                
+                    <li key={todo.id} style={{margin: "10px 0"}}>
+                        {editingId === todo.id ? (
+                            <>
+                                <input
+                                    type="text"
+                                    value={editingText}
+                                    onChange={(event) => setEditingText(event.target.value)}
+                                />
+                                <button onClick={saveEdit}>Salvar</button>
+                            </>
+                        ) : (
+                            <>
+                                {todo.text}
+                                <button onClick={() => startEditing(todo.id, todo.text)}>Editar</button>
+                                <button onClick={() => startEditing(todo.id, todo.text)}>Excluir</button>
+
+                            </>
+
+
+                        ) }
+                    </li>
+    ))
+                }
+
+            </url>
+        </div>
+    );
+
+}
+
+export default TodoList;
+
