03a9e47b1633f2f779e28b562dbdc06aa5f51e16
Julia Oliveira <juliaoliveira.amora@gmail.com>
2025-05-13 09:16:11 -0300

    TextInput basic examples

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -1,46 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button ,Image, TextInput, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, 
+} from 'react-native';
 
-import List from './components/List';
+import Inputs from './components/Inputs';
 
 export default function App() {
 
-return (
-  <View style={styles.container}>
-    <List />
-      <View style={styles.purplebox}></View>
-      <View style={styles.bluebox}></View>
-      <View style={styles.pinkbox}></View>
-     </View>
-  );
+  return (
+    <View style={styles.container}>
+      <Inputs />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-   flexDirection: 'column',
-   justifyContent: 'space-between',
-   alignItems: 'center',
-   backgroundColor: '#E6E6FA',
-   height: 600,
-   marginTop: 150,
-  },
-purplebox: {
-  width: 100,
-  height: 100,
-  backgroundColor: '#4b204b',
-  borderRadius: 50,
-},
-bluebox: {
-  width: 100,
-  height: 100,
-  backgroundColor: '#191970'
-},
-pinkbox: {
-  width: 100,
-  height: 100,
-  backgroundColor: '#f17ea1'
-}
-
-
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 600,
+    marginTop: 150,
+  }
 });
