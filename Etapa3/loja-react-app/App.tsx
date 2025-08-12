bd31e841912d53eaaee37a9becada4e921798d15
Julia Oliveira <juliaoliveira.amora@gmail.com>
2025-08-12 08:45:39 -0300

    Inclui ThemeContext

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import { ThemeProvider } from './src/contexts/ThemeContext'; // NOVA
+
+
 import HomeScreen from './src/screens/HomeScreen';
+
 
 export default function App() {
   return (
-    <HomeScreen/>
+    <ThemeProvider>
+       <HomeScreen/>
+    </ThemeProvider>
+   
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +26,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+
+
+
