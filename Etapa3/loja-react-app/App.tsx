17339b41647e3fbbe6aa84082e82a1befdd3173e
Julia Oliveira <juliaoliveira.amora@gmail.com>
2025-09-24 09:55:43 -0300

    Carrinho de compras salvado o estado dos items

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -1,19 +1,22 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-
-import { ThemeProvider } from './src/contexts/ThemeContext';
+import { ThemeProvider } from './src/contexts/ThemeContext'; // NOVA
 // import HomeScreen from './src/screens/HomeScreen';
 import RootNavigator from './src/navigation/RootNavigator';
 import { AuthProvider } from './src/contexts/AuthContext';
+import { ShopProvider } from './src/contexts/ShopContext';
 
 
 export default function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-    <RootNavigator />
-    </AuthProvider>
+        <ShopProvider>
+          <RootNavigator />
+        </ShopProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
@@ -26,10 +29,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
-
-
-
-
-
--- Etapa3/loja-react-app/src/screens/catalog/CatalogScreen.tsx
+++ Etapa3/loja-react-app/src/screens/catalog/CatalogScreen.tsx
@@ -1,12 +1,16 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; // alterado
 import { View, Text, FlatList, StyleSheet} from 'react-native';
 
 import CatalogCard from "./CatalogCard";
 
+// Todo: importar o serviço de recuperação do catalog
 import { getCatalog } from '../../services/catalogService'; 
+
+import { useShop } from "../../contexts/ShopContext";
 
 const CatalogScreen = ({navigation} : any) => {
     const [catalog, setCatalog] = useState<any[]>([]); 
+    const { addToCart } = useShop();
 
     
     useEffect(() => {
@@ -26,13 +30,14 @@ const CatalogScreen = ({navigation} : any) => {
     const handleBuyPress = (product : any) => {
         // 1 - Adicionar ao carrinho
         // 2 - Ir para a tela do carrinho
+        addToCart(product);
         console.log(product);
     };
 
     const renderItem = ({ item }: any) => ( 
         <CatalogCard 
             product={item} 
-            onBuyPress={() => handleBuyPress(item)} 
+            onBuyPress={() => handleBuyPress(item)}
         />
     );
 
