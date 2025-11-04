9198d1e7e9891ee41544edd9b34a0f67111d964e
Julia Oliveira <juliaoliveira.amora@gmail.com>
2025-11-04 09:16:59 -0300

    Inclui tela de checkout

--- Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
+++ Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
@@ -13,6 +13,7 @@ import RegisterScreen from "../screens/RegisterScreen";
 import LoginScreen from "../screens/LoginScreen";
 import CatalogScreen from "../screens/catalog/CatalogScreen";
 import CartScreen from "../screens/cart/CartScreen";
+import CheckoutScreen from "../screens/cart/CheckoutScreen";
 
 const AppStack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<TabParamList>();
@@ -47,7 +48,11 @@ function TabNavigator() {
               options={{title: 'Seu Carrinho', headerShown: true}}
             />
             <Tab.Screen name="Settings" component={HomeScreen} />
-            <Tab.Screen name="Register" component={RegisterScreen} options={{title: "Cadastrar", headerShown: true}}/>
+            <Tab.Screen
+              name="Register"
+              component={RegisterScreen}
+              options={{title: "Cadastrar", headerShown: true}} // novo
+            />
         </Tab.Navigator>
     );
 }
@@ -69,6 +74,11 @@ function StackNavigator() {
         name="Login"
         component={LoginScreen}
         options={{ title: "Acessar" }}
+      />
+      <AppStack.Screen 
+        name="Checkout"
+        component={CheckoutScreen}
+        options={{title: "Concluir pedido"}}
       />
     </AppStack.Navigator>
   );
