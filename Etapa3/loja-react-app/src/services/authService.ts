cf020f58fa165d39e0eb2155f38daccc050190af
Julia Oliveira <juliaoliveira.amora@gmail.com>
2025-11-18 09:55:13 -0300

    Extrai dados do usuário token

--- Etapa3/loja-react-app/src/services/authService.ts
+++ Etapa3/loja-react-app/src/services/authService.ts
@@ -34,15 +34,15 @@ export async function requestLogin(email: string, password: string): Promise<str
     }
 }
 
-export async function requestRegister(name: string,email: string, password: string): Promise<string> {
+// dupliquei o requestLogin, renomeie e inclui o name nos argumentos.
+export async function requestRegister(name: string, email: string, password: string): Promise<string> {
     try {
-        // alterado 
-        const response = await fetch(`${apiUrl}/api/users`, {
+        const response = await fetch(`${apiUrl}/api/users`, { // modifiquei o endpoint
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({name ,email, password}),
+            body: JSON.stringify({name, email, password}), // inclui o name
         });
         const data = await response.json();
         const jwt = data.accessToken;
@@ -56,3 +56,17 @@ export async function requestRegister(name: string,email: string, password: stri
     }
 }
 
+export async function getTokenData(token: string | null): Promise<any[]> {
+    try {
+        if (!token) {
+            return [];
+        }
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const payload = JSON.parse(atob(base64));
+        return payload;
+    } catch (error) {
+        console.error(error);
+        return Promise.reject('Token inválido.');
+    }
+}
